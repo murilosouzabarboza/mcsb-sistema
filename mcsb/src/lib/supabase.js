5fcfd65635f7c28c362da16dd3987dee5f646271
murilosouzabarboza <murilo.souzabarboza@gmail.com>
2026-05-18 15:30:45 -0300

    Update supabase.js

--- mcsb/src/lib/supabase.js
+++ mcsb/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://vxegcikxmhienbncyju.supabase.co'
+const SUPABASE_URL = 'https://vxegcizkxmhienbncyju.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_lTJueXRSO34YPSu1cBZ4kg_FAq8S9GH'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
